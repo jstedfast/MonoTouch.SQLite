05c07aabf59059418bda2b7b3d821675f9dea7f8
Jeffrey Stedfast <fejj@gnome.org>
2012-03-13 22:41:23 -0400

    Added more convenience methods.

--- MonoTouch.SQLite/SQLiteTableViewController.cs
+++ MonoTouch.SQLite/SQLiteTableViewController.cs
@@ -87,28 +87,49 @@ namespace MonoTouch.SQLite
 			TableView.SectionFooterHeight = 0;
 		}
 		
-		protected override int NumberOfSections (UITableView tableView)
+		protected SQLiteTableModel ModelForTableView (UITableView tableView)
 		{
 			if (tableView == SearchDisplayController.SearchResultsTableView)
-				return SearchModel.SectionCount;
+				return SearchModel;
 			else
-				return Model.SectionCount;
+				return Model;
+		}
+		
+		protected NSIndexPath PathForVisibleItem (UITableView tableView, T item)
+		{
+			SQLiteTableModel model = ModelForTableView (tableView);
+			
+			foreach (var path in tableView.IndexPathsForVisibleRows) {
+				var visibleItem = model.GetItem (path.Section, path.Row);
+				
+				if (visibleItem == item)
+					return path;
+			}
+			
+			return null;
+		}
+		
+		public void ReloadRowForItem (UITableView tableView, T item)
+		{
+			var path = PathForVisibleItem (tableView, item);
+			
+			if (path != null)
+				tableView.ReloadRows (new NSIndexPath[] { path }, UITableViewRowAnimation.Automatic);
+		}
+		
+		protected override int NumberOfSections (UITableView tableView)
+		{
+			return ModelForTableView (tableView).SectionCount;
 		}
 		
 		protected override string TitleForHeader (UITableView tableView, int section)
 		{
-			if (tableView == SearchDisplayController.SearchResultsTableView)
-				return SearchModel.SectionTitles[section];
-			else
-				return Model.SectionTitles[section];
+			return ModelForTableView (tableView).SectionTitles[section];
 		}
 		
 		protected override int RowsInSection (UITableView tableView, int section)
 		{
-			if (tableView == SearchDisplayController.SearchResultsTableView)
-				return SearchModel.GetRowCount (section);
-			else
-				return Model.GetRowCount (section);
+			return ModelForTableView (tableView).GetRowCount (section);
 		}
 		
 		protected virtual UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath, T item)
@@ -118,10 +139,7 @@ namespace MonoTouch.SQLite
 		
 		protected T GetItem (UITableView tableView, NSIndexPath indexPath)
 		{
-			if (tableView == SearchDisplayController.SearchResultsTableView)
-				return SearchModel.GetItem (indexPath.Section, indexPath.Row);
-			else
-				return Model.GetItem (indexPath.Section, indexPath.Row);
+			return ModelForTableView (tableView).GetItem (indexPath.Section, indexPath.Row);
 		}
 		
 		protected override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
